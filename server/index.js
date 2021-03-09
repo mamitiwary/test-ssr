068286ddf0f5f5e5a8610ba8e72a111f5a7c3daa
mamitiwary <amit@superpro.ai>
2021-03-09 20:01:30 +0530

    updated build command

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.get('/', (req, res) => {
     const app = ReactDOMServer.renderToString(<App />);
   
-    const indexFile = path.resolve('./build/index.html');
+    const indexFile = path.resolve('./public/index.html');
     fs.readFile(indexFile, 'utf8', (err, data) => {
       if (err) {
         console.error('Something went wrong:', err);
